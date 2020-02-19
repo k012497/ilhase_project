e6ab746dd539d16363b2ed0725ac1fd748e5d9b2
k012497 <k012497@gmail.com>
2020-02-19 15:36:07 +0900

    move script src

--- common/member_page/person/sign/person_sign_form.php
+++ common/member_page/person/sign/person_sign_form.php
@@ -25,6 +25,7 @@ if(isset($_SESSION["username"])){
     <link rel="stylesheet" href="./slider.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js">
     </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" type="text/javascript"></script>
