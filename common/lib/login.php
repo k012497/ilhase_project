33083386751be8819df5bb03d4b000f02df0baf5
Hello <universe.sns@gmail.com>
2020-02-24 15:38:22 +0900

    add comment / set admin name

--- common/lib/login.php
+++ common/lib/login.php
@@ -18,14 +18,11 @@
    $result = mysqli_query($conn, $sql);
 
    $num_match = mysqli_num_rows($result);
-   if($id=="admin"&&$pass=="123456"){
+   if($id=="admin" && $pass=="123456"){
+     // 관리자
      session_start();
      $_SESSION["userid"] = "admin";
-     if($member_type=="person")
-     $_SESSION["username"] = "123456";
-     else {
-       $_SESSION["username"] = "관리자";
-     }
+     $_SESSION["username"] = "관리자";
      $_SESSION["usermember_type"] = "admin";
      $root_path = $_SERVER['DOCUMENT_ROOT'];
      echo("
@@ -33,23 +30,22 @@
          location.href = 'http://".$_SERVER['HTTP_HOST']."/ilhase/admin/admin.php?admin=true';
        </script>
      ");
-   }else if(!$num_match)
-   {
+   }else if(!$num_match){
+     // 등록되지 않은 아이디의 회원일 때, 관리자 비밀번호 틀렸을 때
      echo("
            <script>
              window.alert('등록되지 않은 아이디입니다!')
              history.go(-1)
            </script>
          ");
-    }
-    else
-    {
+    } else {
+      // 일반 회원(개인/기업)일 때
         $row = mysqli_fetch_array($result);
         $db_pass = $row["pw"];
         mysqli_close($conn);
 
-        if($pass != $db_pass)
-        {
+        if($pass != $db_pass){
+          // 회원 비밀번호가 틀렸을 때
            echo("
               <script>
                 window.alert('비밀번호가 틀립니다!')
@@ -57,9 +53,8 @@
               </script>
            ");
            exit;
-        }
-        else
-        {
+        } else {
+          // 로그인 성공
             session_start();
             $_SESSION["userid"] = $row["id"];
             if($member_type=="person")
