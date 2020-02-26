70bbb6ebb9d87920b40b714d207dd041939539a3
k012497 <k012497@gmail.com>
2020-02-26 09:39:04 +0900

    set path

--- member_page/common/member_side_menu.php
+++ member_page/common/member_side_menu.php
@@ -4,10 +4,10 @@
       <h3 id="profile" class="title"><?=$_SESSION['username']?></h3>
       <div id="left_menu">
         <ul>
-        <a href="./edit_info_form.php"><li class="side_menu_item">내 정보수정</li></a>
-        <a href="./apply_history.php"><li class="side_menu_item">지원내역</li></a>
-        <a href="./favorite.php"><li class="side_menu_item">관심공고</li></a>
-        <a href="./notification.php"><li class="side_menu_item">알림</li></a>
+        <a href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/member_page/person/edit_info_form.php"><li class="side_menu_item">내 정보수정</li></a>
+        <a href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/member_page/person/apply_history.php"><li class="side_menu_item">지원내역</li></a>
+        <a href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/member_page/person/favorite.php"><li class="side_menu_item">관심공고</li></a>
+        <a href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/common/lib/logout.php"><li class="side_menu_item">알림</li></a>
         </ul>
       </div>
     </aside>
