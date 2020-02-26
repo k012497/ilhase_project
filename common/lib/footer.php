e5b068a50bbf818dbfbde1b4e5229253b33fb2f1
k012497 <k012497@gmail.com>
2020-02-26 17:34:39 +0900

    modify footer

---
+++ common/lib/footer.php
@@ -0,0 +1,35 @@
+<footer class="py-5" id="footer_box">
+    <div class="container">
+        <div id="footer_info">
+            <h1 class="navbar-brand" id="footer_title">일하세</h1>
+            <ul>
+            <li><a href="#">서비스 소개</a></li>|
+            <li><a href="#">이용약관 및 정책</a></li>
+            </ul>
+            <ul>
+            <li>일하세(대표이사:김소진)</li>|
+            <li>서울특별시 성동구 도선동</li>|
+            <li>개인정보관리자 : 남채현</li>|
+            <li>통신판매번호 : 2020-서울성동-9999</li>
+            </ul>
+            <ul>
+            <li>유료직업소개사업등록번호 : 제2020-12341234-20-5-01234호</li>|
+            <li>사업자등록번호 : F123-45-678</li>|
+            <li>서비스 및 기업 문의 : 02-123-4515</li>
+            </ul>
+        </div>
+        <br /><br />
+        <p class="m-0" id="copyrihgt">Copyright &copy; ilhase 2020</p>
+    </div>
+</footer>
+
+<style>
+    .footer_info {
+        font-size: smaller;
+    }
+    #footer_box, #footer_box a, #footer_box h1 {
+        background-color: #4f5d6b;
+        color: white;
+        border: 0;
+    }
+</style>
