2c362d5246c7b3dc49cd5969e8cf2d2dcf02bcfa
leekanghyun716 <a980721aa@gmail.com>
2020-02-28 14:26:59 +0900

    헤더 알림 변경및 사이드메뉴 변경

--- common/lib/header.php
+++ common/lib/header.php
@@ -33,7 +33,7 @@
           $username=$_GET['username'];
           if(isset($_GET['usermember_type']))
           $member_type=$_GET['usermember_type'];
-          
+
           if(isset($_SESSION['userid']))
             $id   = $_SESSION['userid'];
 
@@ -103,7 +103,7 @@
                  <div class="dropdown-divider"></div>
                  <a class="dropdown-item" href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/member_page/corporate/corporate_applicant_reading.php">지원자 열람</a>
                  <a class="dropdown-item" href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/member_page/corporate/corporate_plan_manager.php">공고 플랜관리</a>
-                 <a class="dropdown-item" href="#">알림</a>
+                 <a class="dropdown-item" href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase\member_page\common\notification.php">알림</a>
                  <div class="dropdown-divider"></div>
                  <a class="dropdown-item" href="http://<?= $_SERVER['HTTP_HOST'];?>/ilhase/common/lib/logout.php">로그아웃</a>
              </div>
