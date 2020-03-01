22fa5faf29f2004efffeee010cd9b5c9b7ce6779
k012497 <k012497@gmail.com>
2020-03-01 16:57:08 +0900

    make zoom_box fixed

--- common/lib/zoom.php
+++ common/lib/zoom.php
@@ -1,32 +1,28 @@
 <div id="zoom_box">
     <ul>
-        <li id="btn_zoomIn" onclick="zoomIn();"><img src="http://<?= $_SERVER["HTTP_HOST"] ?>/ilhase/common/img/zoomIn.png" alt="확대하기"></li>
-        <li id="btn_reset" onclick="zoomReset();">원래<br>크기로</li>
         <li id="btn_zoomOut" onclick="zoomOut();"><img src="http://<?= $_SERVER["HTTP_HOST"] ?>/ilhase/common/img/zoomOut.png" alt="축소하기"></li>
+        <li id="btn_reset" onclick="zoomReset();">원래&nbsp;크기로</li>
+        <li id="btn_zoomIn" onclick="zoomIn();"><img src="http://<?= $_SERVER["HTTP_HOST"] ?>/ilhase/common/img/zoomIn.png" alt="확대하기"></li>
     </ul>
 </div>
 <script>
-
+var distance= 620;
     
- 
     var scale=1;
     var nowZoom=1;
 
     function zoomIn(){
-
         scale +=0.05;
         nowZoom += 0.05;
         if(scale>=1.2 || nowZoom>=1.2){
-
             scale=1.2;
             nowZoom=1.2;
         }
 
         zoom();
-
     }
-    function zoomOut(){
 
+    function zoomOut(){
         scale -= 0.05;
         nowZoom -= 0.05;
         if(scale <=0.95 || scale <= 0.95){
@@ -34,52 +30,50 @@
             nowZoom=0.95;          
         }
         zoom();
-
-
     }
     
     function zoomReset(){
         scale=1;
         nowZoom=1;
         zoom();
-
     }
+
     function zoom() {
         if(scale==0.95 || nowZoom==0.95){
            alert("더이상 축소할 수 없습니다.");
             return;
        }
+
        if(nowZoom==1.2 || nowZoom==1.2){
         alert(" 더이상 확대 할 수 없습니다.");
         return;
        }
-       document.body.style.zoom = nowZoom;
+
+        document.body.style.zoom = nowZoom;
         document.body.style.webkitTransform='scale('+scale+')';
         document.body.style.transformOrigin= '0% 0%';
-       console.log(scale);
+
+        distance = parseInt($('#zoom_box').offset().top);
     }
+
     //쫄쫄이 메뉴
     $(function(){
+        $('#zoom_box').offset({top : $(window).scrollTop() + (innerHeight * 0.9)});
 
-        var zoom_top=$('#zoom_box').offset().top; // zoom_box의 offsetTop
-        console.log(zoom_top);
-
+        $(window).resize(function(){
+            window.innerHeight;
+        });
+        
+        //스크롤 할때
         $(window).scroll(function(){
-            //스크롤 할때
-        if ($(window).scrollTop() > zoom_top ) {
-				// 스크롤탑 - 박스탑
-				// $(window).scrollTop() - box_top
-				// 브라우저 상단에서 박스까지의 거리값
-				$('#zoom_box').stop().animate({
-					'margin-top' : $(window).scrollTop() - zoom_top +50
-				},300);
-			} else {
-				$('#zoom_box').stop().animate({
-					'margin-top' : 0
-				},300);
-		    }
-      });
-
+            if(scale == 1){
+                $('#zoom_box').offset({top : $(window).scrollTop() + (innerHeight * 0.9)});
+            } else if(scale > 1 && scale <= 1.1){
+                $('#zoom_box').offset({top : $(window).scrollTop() + (innerHeight * 0.8)});
+            } else {
+                $('#zoom_box').offset({top : $(window).scrollTop() + (innerHeight * 0.7)});
+            }
+        });
     });
 
 </script>
