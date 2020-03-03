52c89365cfb66a9fd0351b8d7b0e5965175a8009
Hello <universe.sns@gmail.com>
2020-03-03 13:12:02 +0900

    modify image path

--- common/lib/recommend_based_on_location.php
+++ common/lib/recommend_based_on_location.php
@@ -7,11 +7,11 @@ $user_id=$_POST['user_id'];
 if($user_id==""){
     //유저 아이디가 없을 때 api 오류났을떄 디폴트 '서울 강남구' 로 찾기
     if($location==="서울 강남구") {
-        
+
         $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%서울%' and work_place like '%강남%' order by num desc";
 
         $find_job_result=mysqli_query($conn,$near_location_sql);
-        
+
         for($i=0 ; $i<4 ; $i++) {
             $row=mysqli_fetch_array($find_job_result);
             $num=$row['num'];
@@ -22,14 +22,14 @@ if($user_id==""){
             $period_end=$row['period_end'];
             $work_place=$row['work_place'];
             $file_name=$row['file_name'];
-        
+
             $src='';
             if ($file_name) {
-            $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/"+$file_name;
+            $src="http://".$_SERVER['HTTP_HOST']."/ilhase/manage_articles/data/"+$file_name;
             }else {
             $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/basicimg.jpg";
             }
-        
+
             if($num==""){
                 echo "가깝게 있는 구직공고가 없습니다!";
                 exit;
@@ -47,10 +47,10 @@ if($user_id==""){
                     </div>
                 </div>
             ";
-        
-        
+
+
             }//end of $num==''
-        
+
         }//end of for구문
         exit;
     }//end of 디폴트 if문
@@ -68,14 +68,14 @@ if($user_id==""){
     change_area(); //sql 문을 위한 지역 변환 ex) 서울특별시 -> 서울
     $near_location_sql='';
     if(count($location_str)>=3){
-        $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%$si_do%' and work_place like '%$si_gun_gu%' order by num desc";        
+        $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%$si_do%' and work_place like '%$si_gun_gu%' order by num desc";
     }else {
         $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%$si_do%' or work_place like '%$si_gun_gu%' order by num desc";
     }
-    
+
 
     $find_job_result=mysqli_query($conn,$near_location_sql);
-    
+
     for($i=0 ; $i<4 ; $i++) {
         $row=mysqli_fetch_array($find_job_result);
         $num=$row['num'];
@@ -86,14 +86,14 @@ if($user_id==""){
         $period_end=$row['period_end'];
         $work_place=$row['work_place'];
         $file_name=$row['file_name'];
-    
+
         $src='';
         if ($file_name) {
           $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/"+$file_name;
         }else {
           $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/basicimg.jpg";
         }
-    
+
         if($num==""){
             echo "가깝게 있는 구직공고가 없습니다!";
             exit;
@@ -111,22 +111,22 @@ if($user_id==""){
                 </div>
             </div>
         ";
-    
-    
+
+
         }//end of $num==''
-       
+
     }//end of for구문
-    
+
 
 }else {
 
     //유저 아이디가 있을때 api 오류났을떄 디폴트 '서울 강남구' 로 찾기
     if($location==="서울 강남구") {
-        
+
         $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%서울%' and work_place like '%강남구%' order by num desc";
 
         $find_job_result=mysqli_query($conn,$near_location_sql);
-        
+
         for($i=0 ; $i<4 ; $i++) {
             $row=mysqli_fetch_array($find_job_result);
             $num=$row['num'];
@@ -137,14 +137,14 @@ if($user_id==""){
             $period_end=$row['period_end'];
             $work_place=$row['work_place'];
             $file_name=$row['file_name'];
-        
+
             $src='';
             if ($file_name) {
             $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/"+$file_name;
             }else {
             $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/basicimg.jpg";
             }
-        
+
             if($num==""){
                 echo "가깝게 있는 구직공고가 없습니다!";
                 exit;
@@ -162,10 +162,10 @@ if($user_id==""){
                     </div>
                 </div>
             ";
-        
-        
+
+
             }//end of $num==''
-        
+
         }//end of for구문
         exit;
     }//end of 디폴트 if문
@@ -184,7 +184,7 @@ if($user_id==""){
     $near_location_sql="select num, c.b_name, title, pay, period_start, period_end, work_place, file_name from corporate c join recruitment r on c.id = r.corporate_id where work_place like '%$user_si_do%' and work_place like '%$user_si_gun_gu%' order by num desc";
 
     $find_job_result=mysqli_query($conn,$near_location_sql);
-    
+
     for($i=0 ; $i<4 ; $i++) {
         $row=mysqli_fetch_array($find_job_result);
         $num=$row['num'];
@@ -195,14 +195,14 @@ if($user_id==""){
         $period_end=$row['period_end'];
         $work_place=$row['work_place'];
         $file_name=$row['file_name'];
-    
+
         $src='';
         if ($file_name) {
           $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/"+$file_name;
         }else {
           $src="http://".$_SERVER['HTTP_HOST']."/ilhase/common/img/basicimg.jpg";
         }
-    
+
         if($num==""){
             echo "가깝게 있는 구직공고가 없습니다!";
             exit;
@@ -220,69 +220,69 @@ if($user_id==""){
                 </div>
             </div>
         ";
-    
-    
+
+
         }
-       
+
     }
-    
+
 
 
 }//end of if(user_id)
 
-//주소 변환   
+//주소 변환
 function change_area(){
     global $si_do;
         switch ($si_do) {
-            case '서울특별시': 
+            case '서울특별시':
                     $si_do='서울';
                     break;
-            case '부산광역시': 
+            case '부산광역시':
                     $si_do='부산';
                     break;
-            case '대구광역시': 
+            case '대구광역시':
                     $si_do='대구';
                     break;
-            case '인천광역시': 
+            case '인천광역시':
                     $si_do='인천';
                     break;
-            case '광주광역시': 
+            case '광주광역시':
                     $si_do='광주';
                     break;
-            case '대전광역시': 
+            case '대전광역시':
                     $si_do='대전';
                     break;
-            case '울산광역시': 
+            case '울산광역시':
                     $si_do='울산';
                     break;
-            case '세종특별자치시': 
+            case '세종특별자치시':
                     $si_do='세종';
                     break;
-            case '경기도': 
+            case '경기도':
                     $si_do='경기';
                     break;
-            case '강원도': 
+            case '강원도':
                     $si_do='강원';
                     break;
-            case '충청북도': 
+            case '충청북도':
                     $si_do='충북';
                     break;
-            case '충청남도': 
+            case '충청남도':
                 $si_do='충남';
                 break;
-            case '전라북도': 
+            case '전라북도':
                 $si_do='전북';
                 break;
-            case '전라남도': 
+            case '전라남도':
                 $si_do='전남';
                 break;
-            case '경상북도': 
+            case '경상북도':
                 $si_do='경북';
                 break;
-            case '경상남도': 
+            case '경상남도':
                 $si_do='경북';
                 break;
-            case '제주특별자치도': 
+            case '제주특별자치도':
                 $si_do='제주';
                 break;
         }
