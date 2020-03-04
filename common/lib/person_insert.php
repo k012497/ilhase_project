e5a27682f9ba46cfebd900a73c982ca7c0d1d00c
k012497 <k012497@gmail.com>
2020-03-04 17:15:46 +0900

    modify person sign up form

--- common/lib/person_insert.php
+++ common/lib/person_insert.php
@@ -1,4 +1,6 @@
 <?php
+    include $_SERVER["DOCUMENT_ROOT"]."/ilhase/common/lib/db_connector.php";
+
     $id   = $_POST['id1'];
     $pass = $_POST['pass'];
     $name = $_POST["name"];
@@ -9,17 +11,12 @@
     $roadAddress = $_POST["roadAddress"];
     $jibunAddress = $_POST["jibunAddress"];
     $email  = $_POST["email"];
-    // date_default_timezone_set("Asia/Seoul");
-    // $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
-
-
-    $con = mysqli_connect("localhost", "root", "123456", "ilhase");
 
 	$sql = "insert into person(id, pw, name, birth, email, phone, gender,zipcode,new_address,old_address) ";
 	$sql .= "values('{$id}', '{$pass}', '{$name}', '{$birth}', '{$email}', '{$phone}', {$gender},{$postcode},'{$roadAddress}','{$jibunAddress}')";
 
-	mysqli_query($con, $sql);  // $sql 에 저장된 명령 실행
-    mysqli_close($con);
+	mysqli_query($conn, $sql);  // $sql 에 저장된 명령 실행
+    mysqli_close($conn);
 
     echo "
 	      <script>
